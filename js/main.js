bf5bf807a529b2354d586e73a03355995952f321
ossamaabdein <ossamaabdein@gmail.com>
2021-09-02 02:26:39 +0200

    semi-final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
+
+// declaring variables for manipulation
 var nameInput = document.getElementById("nameInput");
 var emailInput = document.getElementById("emailInput");
 var passwordInput = document.getElementById("passwordInput");
 var signupBtn = document.getElementById("signupBtn");
-var loginBtn = document.getElementById("loginBtn");
 var nameAlert = document.getElementById("nameAlert");
 var emailAlert = document.getElementById("emailAlert");
 var passwordAlert = document.getElementById("passwordAlert");
+var loginMailInput = document.getElementById("loginMailInput");
+var loginPasswordInput = document.getElementById("loginPasswordInput");
+var loginBtn = document.getElementById("loginBtn");
+var loginAlert = document.getElementById("loginAlert");
+var userName = document.getElementById("userName");
+var inputs = document.getElementsByTagName("input");
+
+// var bla;
+// var currentName;
 
 var users = [];     
-var inputs = document.getElementsByTagName("input");
 
 
 if(localStorage.getItem("usersList") != null) {
@@ -27,14 +36,31 @@ function addData() {
 }
 
 
-signupBtn.onclick = function() {
+signupBtn.addEventListener("click", function() {
     if (nameInput.classList.contains("is-valid") && emailInput.classList.contains("is-valid") && passwordInput.classList.contains("is-valid")) {
-        addData();
-        clearInputs();
+        if(CheckRepeatedMail() == false) {
+            emailAlert.innerHTML = "This email is used, try another one.";
+            emailAlert.classList.remove("d-none");
+        } else {
+            addData();
+            clearInputs();
+            emailAlert.classList.add("d-none");
+        }
     }
-   
-    
+});   
+
+
+function CheckRepeatedMail() {
+        for (var i =0; i < users.length; i++) {
+            if(emailInput.value == users[i].email) {
+                return false;
+            } else {
+                console.log(emailInput.value);
+                return true;
+            }
+        }
 }
+
 
 function clearInputs(){
     for(var i = 0; i < inputs.length; i++) {
@@ -44,8 +70,25 @@ function clearInputs(){
     }
 }
 
+//  validate user name
+nameInput.onkeyup = function checkName() {
+    var nameChecker = /^[A-Z0-9a-z_]{3,20}$/gm;
+    if(nameChecker.test(nameInput.value)) {
+        nameInput.classList.add("is-valid");
+        nameInput.classList.remove("is-invalid");
+        nameAlert.classList.add("d-none");
+        return true;
+    } else {
+        nameInput.classList.remove("is-valid");
+        nameInput.classList.add("is-invalid");
+        nameAlert.classList.remove("d-none");
+        return false;
+    }
+}
+
+//  validate user email 
 emailInput.onkeyup = function checkMail() {
-    var emailchecker = /^[A-Z0-9a-z._%+-]+@[A-Za-z0-9]+[.][A-Za-z]{2,20}$/gm;
+    var emailchecker = /^[a-z0-9._]+@[A-Za-z0-9]+[.][A-Za-z]{2,20}$/gm;
     if(emailchecker.test(emailInput.value)) {
         emailInput.classList.add("is-valid");
         emailInput.classList.remove("is-invalid");
@@ -59,6 +102,7 @@ emailInput.onkeyup = function checkMail() {
     }
 }
 
+//  validate user password
 passwordInput.onkeyup = function checkPassword() {
     var passChecker = /^[A-Z0-9a-z._@]{6,20}$/gm;
     if(passChecker.test(passwordInput.value)) {
@@ -74,17 +118,28 @@ passwordInput.onkeyup = function checkPassword() {
     }
 }
 
-nameInput.onkeyup = function checkName() {
-    var nameChecker = /^[A-Z0-9a-z_]{3,20}$/gm;
-    if(nameChecker.test(nameInput.value)) {
-        nameInput.classList.add("is-valid");
-        nameInput.classList.remove("is-invalid");
-        nameAlert.classList.add("d-none");
-        return true;
-    } else {
-        nameInput.classList.remove("is-valid");
-        nameInput.classList.add("is-invalid");
-        nameAlert.classList.remove("d-none");
-        return false;
+
+
+
+
+// Check if user already has an account then login
+function login() {
+    for (var i =0; i < users.length; i++) {
+        if(loginMailInput.value == users[i].email && loginPasswordInput.value ==  users[i].password) {
+            loginAlert.classList.add("d-none");
+            // navigate to home page
+            window.location.href = "successfulLogin.html";
+            bla = i;
+            console.log(users[i].name)
+            currentName = users[i].name;
+            // exit once a matching occurs
+            return;
+        }
+        else {
+            loginAlert.classList.remove("d-none");
+        }
     }
 }
+
+userName.innerHTML = currentName;
+console.log(currentName);
